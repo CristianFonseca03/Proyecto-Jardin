ba1257ee2e4f692d9a5ab6309d51ea0b6b5568e7
cristianfonseca03 <cristian.lfs@gmail.com>
2021-03-05 15:35:12 +0000

    Refactor game

--- src/games/cartas/script.js
+++ src/games/cartas/script.js
@@ -1,69 +1,196 @@
-let animals = [
-    {
+let allAnimals = [{
+    id: 1, image: {
         id: 1,
         parentId: 1,
         media: '../../images/animales/caballo.png',
         type: 'image'
-    },
-    {
+    }, text: {
         id: 2,
         parentId: 1,
         media: 'Caballo',
         type: 'text'
     },
-    {
+}, {
+    id: 2, image: {
         id: 3,
         parentId: 2,
         media: '../../images/animales/cabra.png',
         type: 'image'
-    },
-    {
+    }, text: {
         id: 4,
         parentId: 2,
         media: 'Cabra',
         type: 'text'
     },
-    {
+}, {
+    id: 3, image: {
         id: 5,
         parentId: 3,
         media: '../../images/animales/canguro.png',
         type: 'image'
-    },
-    {
+    }, text: {
         id: 6,
         parentId: 3,
         media: 'Canguro',
         type: 'text'
     },
-    {
+}, {
+    id: 4, image: {
         id: 7,
         parentId: 4,
         media: '../../images/animales/caracol.png',
         type: 'image'
-    },
-    {
+    }, text: {
         id: 8,
         parentId: 4,
         media: 'Caracol',
         type: 'text'
     },
-    {
+}, {
+    id: 5, image: {
         id: 9,
         parentId: 5,
         media: '../../images/animales/castor.png',
         type: 'image'
-    },
-    {
+    }, text: {
         id: 10,
         parentId: 5,
         media: 'Castor',
         type: 'text'
     },
-];
+}, {
+    id: 6, image: {
+        id: 11,
+        parentId: 6,
+        media: '../../images/animales/cerdo.png',
+        type: 'image'
+    }, text: {
+        id: 12,
+        parentId: 6,
+        media: 'Cerdo',
+        type: 'text'
+    },
+}, {
+    id: 7, image: {
+        id: 13,
+        parentId: 7,
+        media: '../../images/animales/cocodrilo.png',
+        type: 'image'
+    }, text: {
+        id: 14,
+        parentId: 7,
+        media: 'Cocodrilo',
+        type: 'text'
+    },
+}, {
+    id: 8, image: {
+        id: 15,
+        parentId: 8,
+        media: '../../images/animales/conejo.png',
+        type: 'image'
+    }, text: {
+        id: 16,
+        parentId: 8,
+        media: 'Conejo',
+        type: 'text'
+    },
+}, {
+    id: 9, image: {
+        id: 17,
+        parentId: 9,
+        media: '../../images/animales/gallina.png',
+        type: 'image'
+    }, text: {
+        id: 18,
+        parentId: 9,
+        media: 'Gallina',
+        type: 'text'
+    },
+}, {
+    id: 10, image: {
+        id: 19,
+        parentId: 10,
+        media: '../../images/animales/hipopotamo.png',
+        type: 'image'
+    }, text: {
+        id: 20,
+        parentId: 10,
+        media: 'Hipopotamo',
+        type: 'text'
+    },
+}, {
+    id: 11, image: {
+        id: 21,
+        parentId: 11,
+        media: '../../images/animales/leon.png',
+        type: 'image'
+    }, text: {
+        id: 22,
+        parentId: 11,
+        media: 'Leon',
+        type: 'text'
+    },
+}, {
+    id: 12, image: {
+        id: 23,
+        parentId: 12,
+        media: '../../images/animales/leopardo.png',
+        type: 'image'
+    }, text: {
+        id: 24,
+        parentId: 12,
+        media: 'Leopardo',
+        type: 'text'
+    },
+}, {
+    id: 13, image: {
+        id: 25,
+        parentId: 13,
+        media: '../../images/animales/loro-2.png',
+        type: 'image'
+    }, text: {
+        id: 26,
+        parentId: 13,
+        media: 'Loro',
+        type: 'text'
+    },
+}, {
+    id: 14, image: {
+        id: 27,
+        parentId: 14,
+        media: '../../images/animales/oso-panda.png',
+        type: 'image'
+    }, text: {
+        id: 28,
+        parentId: 14,
+        media: 'Oso\nPanda',
+        type: 'text'
+    },
+}, {
+    id: 15, image: {
+        id: 29,
+        parentId: 15,
+        media: '../../images/animales/oveja.png',
+        type: 'image'
+    }, text: {
+        id: 30,
+        parentId: 15,
+        media: 'Oveja',
+        type: 'text'
+    },
+}]
+let animals = []
 let cache = []
 let score = 0
 
 $(document).ready(() => {
+    allAnimals.sort(() => {
+        return Math.random() - 0.5
+    });
+    allAnimals.forEach((animal)=>{
+        animals.push(animal.text)
+        animals.push(animal.image)
+    })
     animals.sort(() => {
         return Math.random() - 0.5
     });
@@ -97,7 +224,7 @@ const textCard = (animal) => {
             <div class="front animal-card-front">
             </div>
             <div class="back animal-card-back d-flex flex-column justify-content-center">
-                <h1 class="text-center">${animal.media}</h1>
+                <h1 class="text-center animal-card-text">${animal.media}</h1>
             </div>
         </div>`
 }
