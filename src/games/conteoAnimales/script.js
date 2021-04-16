d90d5acc4414b23c91e33c7c522d088bb2de8a89
cristianfonseca03 <cristian.lfs@gmail.com>
2021-04-16 17:22:44 +0000

    Add games to index

--- src/games/conteoAnimales/script.js
+++ src/games/conteoAnimales/script.js
@@ -24,48 +24,48 @@ function Jugar() {
     var tabla = document.getElementById('TablaJuego');
     tabla.innerHTML = '';
 
-        var count = 0;
-        for (let index = 0; index < RandomNumero; index++) {
-            count++;
+    var count = 0;
+    for (let index = 0; index < RandomNumero; index++) {
+        count++;
 
-             if (count <= 3) {
+        if (count <= 3) {
 
-                tabla.innerHTML += `
+            tabla.innerHTML += `
                 
             <img class="imgRedonda animate__animated animate__backInDown animate__delay-1s" src="../../images/animales/${animales[RandomAnimal]}"> 
               
                 `
 
-            }else{
-                tabla.innerHTML += `
+        }else{
+            tabla.innerHTML += `
               <p></p>  
              <img class="imgRedonda animate__animated animate__backInDown animate__delay-1s" src="../../images/animales/${animales[RandomAnimal]}"> 
                
                 `
-                count = 1;
-            }
-
-
-
+            count = 1;
         }
 
 
 
-        if (RandomBottonDesc==1) {
-            tabla.innerHTML += `
+    }
+
+
+
+    if (RandomBottonDesc==1) {
+        tabla.innerHTML += `
             <p></p>  
             <ul class="actions CuerpoLogin">
 	
-            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
+            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
     
-            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
+            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
 
-            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
+            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
 
-            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
+            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
             
 
-            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
+            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
 
     
 									
@@ -76,110 +76,127 @@ function Jugar() {
 
              
               `
-            }
+    }
 
-            if (RandomBottonDesc==2) {
-                tabla.innerHTML += `
+    if (RandomBottonDesc==2) {
+        tabla.innerHTML += `
                 <p></p>  
         
                 <ul class="actions CuerpoLogin">
     
-                <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
-                <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
-                <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
+                <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
+                <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
+                <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
 
-                <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
+                <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
                 
 
-                <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
+                <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
                 </ul>
     
                  
                   `
-                }
-                if (RandomBottonDesc==3) {
-                    tabla.innerHTML += `
+    }
+    if (RandomBottonDesc==3) {
+        tabla.innerHTML += `
                     <p></p>  
                     <ul class="actions CuerpoLogin">
     
-                    <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
-                    <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
-                    <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
-                    <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
+                    <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
+                    <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
+                    <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
+                    <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
                     
 
-                    <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
+                    <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
         
             
                     </ul>
                      
                       `
-                    }
-                    if (RandomBottonDesc==4) {
-                        tabla.innerHTML += `
+    }
+    if (RandomBottonDesc==4) {
+        tabla.innerHTML += `
                         <p></p>  
                         <ul class="actions CuerpoLogin">
         
-                        <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
-                        <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
-                        <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
+                        <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
+                        <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
+                        <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
                         
-                        <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
-                        <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
+                        <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
+                        <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
 
             
                 
                         </ul>
                          
                           `
-                        }
+    }
 
-                        if (RandomBottonDesc==5) {
-                            tabla.innerHTML += `
+    if (RandomBottonDesc==5) {
+        tabla.innerHTML += `
                             <p></p>  
                             <ul class="actions CuerpoLogin">
             
-                            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
-                            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
-                            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
-                            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
+                            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton1})">${RandomBotton1}</a></li>
+                            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton2})">${RandomBotton2}</a></li>
+                            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton3})">${RandomBotton3}</a></li>
+                            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomBotton4})">${RandomBotton4}</a></li>
 
-                            <li><a class="buttonJuego1" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
+                            <li><a class="button" onclick="Ganador(${RandomNumero} , ${RandomNumero})">${RandomNumero}</a></li>
                 
                             </ul>
                 
                              
                               `
-                            }
-
-
-
-
-
     }
 
 
 
 
-function Ganador(Numero1 , Numero2){
 
-  var Puntaje = document.getElementById('Puntaje');
-  Puntaje.innerHTML = '';
-
-if (Numero1 == Numero2) {
-
-  alert('Felicidades ');
-  puntos++;
-  Puntaje.innerHTML += `Puntaje : ${puntos} `
-
-  Jugar();
-
-}else{
-
-  alert('ups el numero correcto es el '+Numero1);
-  Puntaje.innerHTML += `Puntaje : ${puntos} `
-  Jugar();
 }
+
+
+
+
+function Ganador(Numero1 , Numero2){
+    var modalBody = document.getElementById('modalBody');
+    var Puntaje = document.getElementById('Puntaje');
+    Puntaje.innerHTML = '';
+
+    if (Numero1 == Numero2) {
+        puntos++;
+
+        modalBody.innerHTML = '';
+        modalBody.innerHTML += `
+  <img class="imgModal animate__animated animate__backInDown animate__delay-1s" src="../../images/Estrella.png"> 
+  <h3>! ! Felicidades ¡ ¡ </h3>
+  <h1 class="section-tittle">Puntaje : ${puntos}</h1> 
+     `
+
+        $('#Modal_Puntaje').modal('show');
+
+
+
+        Puntaje.innerHTML += `Puntaje : ${puntos} `
+
+        Jugar();
+
+    }else{
+
+        modalBody.innerHTML = '';
+        modalBody.innerHTML += `
+  <img class=" imgModal animate__animated animate__backInDown animate__delay-1s" src="../../images/CaraTriste.png"> 
+  <h3>ups!! el numero correcto es : " ${Numero1} " </h3>
+    
+     `
+        $('#Modal_Puntaje').modal('show');
+
+        Puntaje.innerHTML += `Puntaje : ${puntos} `
+        Jugar();
+    }
 
 
 
