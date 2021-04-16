20582f205d6eb1836a591ac37b0a3a2bddc0cb18
cristianfonseca03 <cristian.lfs@gmail.com>
2021-04-16 02:36:13 +0000

    Change font of cards, adapt to new design

--- src/games/cartas/script.js
+++ src/games/cartas/script.js
@@ -106,93 +106,93 @@ let allAnimals = [{
         media: 'Gallina',
         type: 'text'
     },
-},
-//     {
-//     id: 10, image: {
-//         id: 19,
-//         parentId: 10,
-//         media: '../../images/animales/hipopotamo.png',
-//         type: 'image'
-//     }, text: {
-//         id: 20,
-//         parentId: 10,
-//         media: 'Hipopotamo',
-//         type: 'text'
-//     },
-// }, {
-//     id: 11, image: {
-//         id: 21,
-//         parentId: 11,
-//         media: '../../images/animales/leon.png',
-//         type: 'image'
-//     }, text: {
-//         id: 22,
-//         parentId: 11,
-//         media: 'Leon',
-//         type: 'text'
-//     },
-// }, {
-//     id: 12, image: {
-//         id: 23,
-//         parentId: 12,
-//         media: '../../images/animales/leopardo.png',
-//         type: 'image'
-//     }, text: {
-//         id: 24,
-//         parentId: 12,
-//         media: 'Leopardo',
-//         type: 'text'
-//     },
-// }, {
-//     id: 13, image: {
-//         id: 25,
-//         parentId: 13,
-//         media: '../../images/animales/loro-2.png',
-//         type: 'image'
-//     }, text: {
-//         id: 26,
-//         parentId: 13,
-//         media: 'Loro',
-//         type: 'text'
-//     },
-// }, {
-//     id: 14, image: {
-//         id: 27,
-//         parentId: 14,
-//         media: '../../images/animales/oso-panda.png',
-//         type: 'image'
-//     }, text: {
-//         id: 28,
-//         parentId: 14,
-//         media: 'Oso\nPanda',
-//         type: 'text'
-//     },
-// }, {
-//     id: 15, image: {
-//         id: 29,
-//         parentId: 15,
-//         media: '../../images/animales/oveja.png',
-//         type: 'image'
-//     }, text: {
-//         id: 30,
-//         parentId: 15,
-//         media: 'Oveja',
-//         type: 'text'
-//     },
-// }
+}, {
+    id: 10, image: {
+        id: 19,
+        parentId: 10,
+        media: '../../images/animales/hipopotamo.png',
+        type: 'image'
+    }, text: {
+        id: 20,
+        parentId: 10,
+        media: 'Hipopotamo',
+        type: 'text'
+    },
+}, {
+    id: 11, image: {
+        id: 21,
+        parentId: 11,
+        media: '../../images/animales/leon.png',
+        type: 'image'
+    }, text: {
+        id: 22,
+        parentId: 11,
+        media: 'Leon',
+        type: 'text'
+    },
+}, {
+    id: 12, image: {
+        id: 23,
+        parentId: 12,
+        media: '../../images/animales/leopardo.png',
+        type: 'image'
+    }, text: {
+        id: 24,
+        parentId: 12,
+        media: 'Leopardo',
+        type: 'text'
+    },
+}, {
+    id: 13, image: {
+        id: 25,
+        parentId: 13,
+        media: '../../images/animales/loro-2.png',
+        type: 'image'
+    }, text: {
+        id: 26,
+        parentId: 13,
+        media: 'Loro',
+        type: 'text'
+    },
+}, {
+    id: 14, image: {
+        id: 27,
+        parentId: 14,
+        media: '../../images/animales/oso-panda.png',
+        type: 'image'
+    }, text: {
+        id: 28,
+        parentId: 14,
+        media: 'Oso\nPanda',
+        type: 'text'
+    },
+}, {
+    id: 15, image: {
+        id: 29,
+        parentId: 15,
+        media: '../../images/animales/oveja.png',
+        type: 'image'
+    }, text: {
+        id: 30,
+        parentId: 15,
+        media: 'Oveja',
+        type: 'text'
+    },
+}
 ]
 let animals = []
 let cache = []
 let score = 0
+let numberAnimals = 12
 
 $(document).ready(() => {
     allAnimals.sort(() => {
         return Math.random() - 0.5
     });
-    allAnimals.forEach((animal)=>{
-        animals.push(animal.text)
-        animals.push(animal.image)
-    })
+    for (let i = 0; i < numberAnimals; i++) {
+        animals.push(allAnimals[i].text)
+        animals.push(allAnimals[i].image)
+    }
     animals.sort(() => {
         return Math.random() - 0.5
     });
@@ -201,7 +201,7 @@ $(document).ready(() => {
 })
 
 const refreshScore = () => {
-    $('#score').text(`Puntaje ${score}`)
+    $('#score').text(`Animales encontrados: ${score}/${animals.length / 2}`)
 }
 
 const createCards = (animals) => {
@@ -212,17 +212,17 @@ const createCards = (animals) => {
 }
 
 const imageCard = (animal) => {
-    return `<div id="${animal.id}" class="animal-card col-2 m-2" onclick="handleClickCard(this)">
+    return `<div id="${animal.id}" class="animal-card m-2" onclick="handleClickCard(this)">
             <div class="front animal-card-front">
             </div>
             <div class="back animal-card-back d-flex flex-row justify-content-center">
-                <img src="${animal.media}" class="img-fluid" alt="${animal.id}">
+                <img src="${animal.media}" class="img-fluid animal-image"  alt="${animal.id}">
             </div>
         </div>`
 }
 
 const textCard = (animal) => {
-    return `<div id="${animal.id}" class="animal-card col-2 m-2" onclick="handleClickCard(this)">
+    return `<div id="${animal.id}" class="animal-card m-2" onclick="handleClickCard(this)">
             <div class="front animal-card-front">
             </div>
             <div class="back animal-card-back d-flex flex-column justify-content-center">
@@ -242,6 +242,9 @@ const handleClickCard = (e) => {
                 score++
                 refreshScore()
                 cache = []
+                if (score === animals.length / 2) {
+                    $('#modal').modal('toggle')
+                }
             } else {
                 cache.forEach(animal => handleFlipCard(animal.id))
                 cache = []
